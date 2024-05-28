f442bfc47d79d9627ed0af41067bab7b353c5675
KJZH001 <2802007619@qq.com>
2024-05-28 17:27:56 +0800

    更改命名空间

--- LittleSkinExtendSocialite.php
+++ LittleSkinExtendSocialite.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace LittleSkinChina\BsSocialiteProviderLittleSkin;
+namespace LittleSkinChina\BsSocialiteProviderMoeWorld;
 
 use SocialiteProviders\Manager\SocialiteWasCalled;
 
--- Provider.php
+++ Provider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace LittleSkinChina\BsSocialiteProviderLittleSkin;
+namespace MoeWorld_CN\BsSocialiteProviderMoeWorld;
 
 use SocialiteProviders\Manager\OAuth2\AbstractProvider;
 use SocialiteProviders\Manager\OAuth2\User;
